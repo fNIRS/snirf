ed994f90094a65990a62f29684bdaf98fe28887b
Johann Benerradi <johann.benerradi@gmail.com>
2026-07-13 15:29:00 +0100

    Add valid SNIRF file

--- validator/test.py
+++ validator/test.py
@@ -1,29 +1,8 @@
-import os
-import urllib.request
-
 from snirf_schema import read_snirf
 
 
-# =============================================================================
-# DOWNLOAD FILE
-# =============================================================================
-filename = "neuro_run01.snirf"
-url = (
-    "https://github.com/fNIRS/snirf-samples/raw/master/"
-    f"basic/{filename}"
-)
+filename = "simple_probe_valid.snirf"
 
-if not os.path.exists(filename):
-    urllib.request.urlretrieve(url, filename)
-
-
-# =============================================================================
-# VALIDATE FILE
-# =============================================================================
 snirf = read_snirf(filename, warnings=True)
 
-
-# =============================================================================
-# WRITE
-# =============================================================================
 snirf.save('new_'+filename)
